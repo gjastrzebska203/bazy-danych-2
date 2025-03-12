a438f019efd35ff7aa78c4345a01198f4fef1306
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-03-12 10:46:41 +0100

    commit 1

---
+++ techmarket-api/src/data/products.js
@@ -0,0 +1,26 @@
+products = [
+  {
+    id: 1,
+    name: "rice",
+    category: "food",
+    description: "",
+    price: 5.55,
+    stockCount: 56,
+  },
+  {
+    id: 2,
+    name: "buckwheat",
+    category: "food",
+    description: "",
+    price: 2.55,
+    stockCount: 34,
+  },
+  {
+    id: 3,
+    name: "cheese",
+    category: "food",
+    description: "",
+    price: 4.55,
+    stockCount: 32,
+  },
+];
---
+++ techmarket-api/src/routes/productRoutes.js
@@ -0,0 +1,3 @@
+"use client";
+const express = require("express");
+const router = express.Router();
